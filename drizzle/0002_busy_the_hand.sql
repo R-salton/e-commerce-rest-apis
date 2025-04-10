8d85012a154edc39fd1d2f8daac50500d8af9ef7
R-salton <ringusa22@gmail.com>
2025-04-10 15:36:21 +0200

    Drizzle setup

---
+++ drizzle/0002_busy_the_hand.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "products" ALTER COLUMN "Image" DROP NOT NULL;
---
+++ src/db/productsSchema.ts
@@ -0,0 +1,11 @@
+import { integer, pgTable, varchar,doublePrecision } from "drizzle-orm/pg-core";
+
+export const productsTable = pgTable("products", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }),
+  description: varchar({ length: 255 }),
+  price: doublePrecision(),
+  Image: varchar({ length: 255 }),
+ 
+
+});
