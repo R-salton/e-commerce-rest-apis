0f9aa100d05f63ef8e98d3c2a157b2981029902f
R-salton <ringusa22@gmail.com>
2025-04-14 09:59:08 +0200

    Added pruduct schema

---
+++ dist/src/db/productsSchema.js
@@ -0,0 +1,13 @@
+import { integer, pgTable, varchar, doublePrecision } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
+import { usersTable } from "./usersSchema";
+export const productsTable = pgTable("products", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    userId: integer().references(() => usersTable.id),
+    name: varchar({ length: 255 }).notNull(),
+    description: varchar({ length: 255 }),
+    price: doublePrecision(),
+    Image: varchar({ length: 255 }),
+});
+export const CreateProductSchema = createInsertSchema(productsTable).omit({ id: true });
+export const updateProductSchema = createInsertSchema(productsTable).omit({ id: true }).partial();
